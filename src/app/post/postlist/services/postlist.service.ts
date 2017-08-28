b158e1d5beb1b3639a43d430350a73f017efbd04
sulishibaobei <380571249@qq.com>
2017-08-28 17:18:58 +0800

    17:18

--- src/app/post/postlist/services/postlist.service.ts
+++ src/app/post/postlist/services/postlist.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
-import { Http, Response, Headers, RequestOptions,URLSearchParams } from '@angular/http';
+import { Http, Response, Headers, RequestOptions, URLSearchParams } from '@angular/http';
 import { Observable } from 'rxjs/Rx';
-
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
 
@@ -12,37 +11,42 @@ export class PostlistService {
   public postListURL = 'mock-data/postlist-mock.json';
   public postListSearchURL = 'mock-data/postlist-search-mock.json';
 
-  constructor(public http:Http) { }
-  
-  public getPostList(searchText: string,page:number=1):Observable<Post[]>{
+  constructor(public http: Http) { }
+
+  public getPostList(searchText: string, page: number = 1): Observable<Post[]> {
     let url = this.postListURL;
+    // URLSearchParams  处理url参数的串
     let params = new URLSearchParams();
+    /**
+     * 验证输入框中是否有值
+     */
     if (searchText) {
-			params.set('searchText',searchText);
+      params.set('searchText', searchText);
       url = this.postListSearchURL;
-      console.log(`searchText=${searchText}`);
-		}
-    params.set('page',String(page));
-    
+      //console.log(`searchText=${searchText}`);
+    }
+    params.set('page', String(page));
+
     return this.http
-               .get(url,{search:params})
-               .map((res:Response) => {
-                   let result=res.json();
-                   console.log(result);
-                   return result;
-               })
-               .catch((error:any) => Observable.throw(error || 'Server error'));
+      .get(url, { search: params })
+      .map((res: Response) => {
+        let result = res.json();
+        console.log(params);
+        return result;
+      })
+      /**  错误操作处理  还有这个Promise.reject(error.message || error)*/
+      .catch((error: any) => Observable.throw(error || 'Server error'));
   }
 
-  public getPostNumber():number{
+  public getPostNumber(): number {
     return 0;
   }
 
-  public addPost(user:any){
+  public addPost(user: any) {
 
   }
 
   public search() {
-    
+
   }
 }
