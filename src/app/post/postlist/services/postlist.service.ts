9d30c8b5a08ce61398380188056437a0e772b297
sulishibaobei <380571249@qq.com>
2017-09-06 15:17:08 +0800

    1

--- src/app/post/postlist/services/postlist.service.ts
+++ src/app/post/postlist/services/postlist.service.ts
@@ -9,10 +9,7 @@ import { Post } from '../../model/post-model';
 @Injectable()
 export class PostlistService {
   public postListURL = 'mock-data/postlist-mock.json';
-  public postListSearchURL = 'mock-data/postlist-search-mock.json';
-
   constructor(public http: Http) { }
-
   public getPostList(searchText: string, page: number = 1): Observable<Post[]> {
     let url = this.postListURL;
     // URLSearchParams  处理url参数的串
@@ -22,22 +19,17 @@ export class PostlistService {
      */
     if (searchText) {
       params.set('searchText', searchText);
-      url = this.postListSearchURL;
-      //console.log(`searchText=${searchText}`);
     }
     params.set('page', String(page));
-
     return this.http
       .get(url, { search: params })
       .map((res: Response) => {
         let result = res.json();
-        console.log(params);
         return result;
       })
       /**  错误操作处理  还有这个Promise.reject(error.message || error)*/
       .catch((error: any) => Observable.throw(error || 'Server error'));
   }
-
   public getPostNumber(): number {
     return 0;
   }
